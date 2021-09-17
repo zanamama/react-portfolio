6888a4d96f2df5a0e1941e8a19503d0412511ea3
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-17 11:11:37 -0400

    changed portfolio.js and about section with info from previous portfolio

--- src/styles/components/pages/About.js
+++ src/styles/components/pages/About.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>About Zana Mathuthu</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Professional Background: Zana Mathuthu graduated from Ursinus College n
+        2012 with a BA degree in Ecomonics, with minors in Mathematics and
+        Statistics. She has 8 years of contracting experience successfully
+        helping companies between $5 Million - $300 Million increase their GP by
+        $1 Million+ per year. These were all technology consulting and staffing
+        companies. In 2019, Zana then began helping entrepreneurs and very small
+        business owners grow their online presence using SEO and digital
+        marketing. Now, with a better understanding of mobile app development
+        and website development, Zana can aid entrepreneurs on more complex
+        projects. As Zana continues to grow her skillset, she is eager to grow
+        her career in sales consulting for technology firms and entrepreneurs.
       </p>
     </div>
   );
---
+++ src/styles/components/pages/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <p>You will see all of my projects here soon.</p>
+    </div>
+  );
+}
