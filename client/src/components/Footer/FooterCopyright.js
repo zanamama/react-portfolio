609546e87d212b3911ce57114d4806c25217d41c
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-27 12:13:03 -0400

    edited footerCopyright.js file adding comments and importing the footer design

---
+++ client/src/components/Footer/FooterCopyright.js
@@ -0,0 +1,11 @@
+import Footer from "./Footer";
+
+const FooterCopyright = () => {
+  return (
+    <footer>
+      <FooterCopyright>©Created 2021 By Zana Mathuthu</FooterCopyright>
+    </footer>
+  );
+};
+
+export default FooterCopyright;
